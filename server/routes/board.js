612bf583978820b73db120de6e0479b960ed8950
tunalad <novica.arsic@aol.com>
2023-05-23 19:48:33 +0200

    implemented a route that returns all board's data

--- server/routes/board.js
+++ server/routes/board.js
@@ -32,6 +32,96 @@ router.get("/:id", (req, res) => {
 	}
 });
 
+router.get("/:id/full", async (req, res) => {
+	try {
+		const itemId = req.params.id;
+
+		const boardQuery = `SELECT * FROM ${tableName} WHERE id=${itemId}`;
+		const listsQuery = `SELECT * FROM list WHERE board_id=${itemId}`;
+		const cardsQuery = `SELECT * FROM card WHERE list_id IN (SELECT id FROM list WHERE board_id=${itemId})`;
+		const labelsQuery = `SELECT * FROM list WHERE board_id=${itemId}`;
+		const cardLabelQuery = `SELECT * FROM card_label WHERE card_id IN (SELECT id FROM card WHERE list_id IN (SELECT id FROM list WHERE board_id=${itemId}))`;
+
+		const getSqlData = (query) => {
+			return new Promise((resolve, reject) => {
+				db.execSql(query, (error, data) => {
+					if (error) {
+						reject(error);
+					} else {
+						resolve(data);
+					}
+				});
+			});
+		};
+
+		const [boardData, listsData, cardsData, labelsData, cardLabelData] =
+			await Promise.all([
+				getSqlData(boardQuery),
+				getSqlData(listsQuery),
+				getSqlData(cardsQuery),
+				getSqlData(labelsQuery),
+				getSqlData(cardLabelQuery),
+			]);
+
+		// returns cards with corresponding labels
+		const nestedCards = cardsData.map((card) => {
+			// connect labels to the corresponding card
+			const nestedLabels = cardLabelData
+				.filter((cardLabel) => cardLabel.card_id === card.id)
+				.map((cardLabel) =>
+					labelsData.find((label) => label.id === cardLabel.label_id)
+				);
+
+			// create a nested card object with associated labels
+			const nestedCard = {
+				...card,
+				labels: nestedLabels,
+			};
+
+			return nestedCard;
+		});
+
+		// returns list with corresponding cards
+		const nestedLists = listsData.map((list) => {
+			// assign the corresponding cards to each list
+			const nestedCardsData = nestedCards.filter(
+				(card) => card.list_id === list.id
+			);
+
+			// create a nested list object with associated cards
+			const nestedList = {
+				...list,
+				cards: nestedCardsData,
+			};
+
+			return nestedList;
+		});
+
+		// returns board with corresponding lists
+		const nestedBoardData = boardData.map((board) => {
+			// assign the corresponding lists to each board
+			const nestedListsData = nestedLists.filter(
+				(list) => list.board_id === board.id
+			);
+
+			// create a nested board object with associated lists and labels
+			const nestedBoard = {
+				...board,
+				lists: nestedListsData,
+				labels: labelsData.filter(
+					(label) => label.board_id === board.id
+				),
+			};
+
+			return nestedBoard;
+		});
+
+		res.status(200).json(nestedBoardData);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
 /* POST */
 router.post("/", (req, res) => {
 	try {
