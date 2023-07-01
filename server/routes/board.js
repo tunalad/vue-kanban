ffb99201e72fed2e1dd41a66d538aea11f19291a
tunalad <novica.arsic@aol.com>
2023-07-01 16:29:11 +0200

    added route that watches board changes
    moved the logic of getting the full board to a separate function

--- server/routes/board.js
+++ server/routes/board.js
@@ -5,6 +5,93 @@ const JWTHelper = require("../utils/jwt-helpers");
 const db = require("../database");
 const tableName = "board";
 
+async function getBoardDataFull(itemId) {
+	// returns full board data
+	const boardQuery = `SELECT * FROM ${tableName} WHERE id=${itemId}`;
+	const listsQuery = `SELECT * FROM list WHERE board_id=${itemId}`;
+	const cardsQuery = `SELECT * FROM card WHERE list_id IN (SELECT id FROM list WHERE board_id=${itemId})`;
+	const labelsQuery = `SELECT * FROM label WHERE board_id=${itemId}`;
+	const cardLabelQuery = `SELECT * FROM card_label WHERE card_id IN (SELECT id FROM card WHERE list_id IN (SELECT id FROM list WHERE board_id=${itemId}))`;
+
+	const getSqlData = (query) => {
+		return new Promise((resolve, reject) => {
+			db.execSql(query, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	};
+
+	const [boardData, listsData, cardsData, labelsData, cardLabelData] =
+		await Promise.all([
+			getSqlData(boardQuery),
+			getSqlData(listsQuery),
+			getSqlData(cardsQuery),
+			getSqlData(labelsQuery),
+			getSqlData(cardLabelQuery),
+		]);
+
+	// Returns cards with corresponding labels
+	const nestedCards = cardsData.map((card) => {
+		// Connect labels to the corresponding card
+		const nestedLabels = cardLabelData
+			.filter((cardLabel) => cardLabel.card_id === card.id)
+			.map((cardLabel) =>
+				labelsData.find((label) => label.id === cardLabel.label_id)
+			);
+
+		// Create a nested card object with associated labels
+		const nestedCard = {
+			...card,
+			labels: nestedLabels,
+		};
+
+		return nestedCard;
+	});
+
+	// Returns lists with corresponding cards
+	const nestedLists = listsData.map((list) => {
+		// Assign the corresponding cards to each list
+		const nestedCardsData = nestedCards.filter(
+			(card) => card.list_id === list.id
+		);
+
+		// Create a nested list object with associated cards
+		const nestedList = {
+			...list,
+			cards: nestedCardsData,
+		};
+
+		return nestedList;
+	});
+
+	// Returns board with corresponding lists and labels
+	const nestedBoardData = boardData.map((board) => {
+		// Assign the corresponding lists to each board
+		const nestedListsData = nestedLists.filter(
+			(list) => list.board_id === board.id
+		);
+
+		// Create a nested board object with associated lists and labels
+		const nestedBoard = {
+			...board,
+			lists: nestedListsData,
+			labels: labelsData.filter((label) => label.board_id === board.id),
+		};
+
+		return nestedBoard;
+	});
+
+	return nestedBoardData;
+}
+
+/**************************/
+/********* ROUTES *********/
+/**************************/
+
 /* GET */
 router.get("/", (req, res) => {
 	try {
@@ -35,89 +122,38 @@ router.get("/:id", (req, res) => {
 router.get("/:id/full", async (req, res) => {
 	try {
 		const itemId = req.params.id;
-
-		const boardQuery = `SELECT * FROM ${tableName} WHERE id=${itemId}`;
-		const listsQuery = `SELECT * FROM list WHERE board_id=${itemId}`;
-		const cardsQuery = `SELECT * FROM card WHERE list_id IN (SELECT id FROM list WHERE board_id=${itemId})`;
-		const labelsQuery = `SELECT * FROM label WHERE board_id=${itemId}`;
-		const cardLabelQuery = `SELECT * FROM card_label WHERE card_id IN (SELECT id FROM card WHERE list_id IN (SELECT id FROM list WHERE board_id=${itemId}))`;
-
-		const getSqlData = (query) => {
-			return new Promise((resolve, reject) => {
-				db.execSql(query, (error, data) => {
-					if (error) {
-						reject(error);
-					} else {
-						resolve(data);
-					}
-				});
-			});
-		};
-
-		const [boardData, listsData, cardsData, labelsData, cardLabelData] =
-			await Promise.all([
-				getSqlData(boardQuery),
-				getSqlData(listsQuery),
-				getSqlData(cardsQuery),
-				getSqlData(labelsQuery),
-				getSqlData(cardLabelQuery),
-			]);
-
-		// returns cards with corresponding labels
-		const nestedCards = cardsData.map((card) => {
-			// connect labels to the corresponding card
-			const nestedLabels = cardLabelData
-				.filter((cardLabel) => cardLabel.card_id === card.id)
-				.map((cardLabel) =>
-					labelsData.find((label) => label.id === cardLabel.label_id)
-				);
-
-			// create a nested card object with associated labels
-			const nestedCard = {
-				...card,
-				labels: nestedLabels,
-			};
-
-			return nestedCard;
-		});
-
-		// returns list with corresponding cards
-		const nestedLists = listsData.map((list) => {
-			// assign the corresponding cards to each list
-			const nestedCardsData = nestedCards.filter(
-				(card) => card.list_id === list.id
-			);
-
-			// create a nested list object with associated cards
-			const nestedList = {
-				...list,
-				cards: nestedCardsData,
-			};
-
-			return nestedList;
-		});
-
-		// returns board with corresponding lists
-		const nestedBoardData = boardData.map((board) => {
-			// assign the corresponding lists to each board
-			const nestedListsData = nestedLists.filter(
-				(list) => list.board_id === board.id
-			);
-
-			// create a nested board object with associated lists and labels
-			const nestedBoard = {
-				...board,
-				lists: nestedListsData,
-				labels: labelsData.filter(
-					(label) => label.board_id === board.id
-				),
-			};
-			return nestedBoard;
-		});
-
+		const nestedBoardData = await getBoardDataFull(itemId);
 		res.status(200).json(nestedBoardData);
 	} catch (error) {
 		res.status(500).json({ error: error.message });
+	}
+});
+
+router.get("/:id/live", async (req, res) => {
+	try {
+		res.setHeader("Content-Type", "text/event-stream");
+
+		const itemId = req.params.id;
+		let data = {
+			boardId: itemId,
+			updateRequired: false,
+		};
+
+		let latestData = await getBoardDataFull(itemId);
+
+		setInterval(async () => {
+			let newLatestData = await getBoardDataFull(itemId);
+
+			if (JSON.stringify(latestData) === JSON.stringify(newLatestData))
+				data.updateRequired = false;
+			else {
+				latestData = newLatestData;
+				data.updateRequired = true;
+			}
+			res.write(`data: ${JSON.stringify(data)}\n\n`);
+		}, 500);
+	} catch (e) {
+		res.status(500).json({ error: e.message });
 	}
 });
 
